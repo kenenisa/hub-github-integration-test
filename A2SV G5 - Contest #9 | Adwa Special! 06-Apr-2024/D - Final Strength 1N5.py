4abf18be0406bad7cc5f6e135d8b50a0a6af8874
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2024-04-06 16:07:29 +0300

    Created 'D - Final Strength' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 06-Apr-2024/D - Final Strength 1N5.py
@@ -0,0 +1,6 @@
+# Problem: D - Final Strength - https://codeforces.com/gym/513152/problem/D
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        df = defaultdict(int)
+       
